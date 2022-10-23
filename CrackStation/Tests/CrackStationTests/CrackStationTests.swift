dd598473ee0bbb4d9e0887560e85f1510f936510
JungCheChang <jungche0408@gmail.com>
2022-10-23 14:08:12 -0700

    POC v1

--- CrackStation/Tests/CrackStationTests/CrackStationTests.swift
+++ CrackStation/Tests/CrackStationTests/CrackStationTests.swift
@@ -2,4 +2,11 @@ import XCTest
 @testable import CrackStation
 
 final class CrackStationTests: XCTestCase {
+    
+    func testCrackStation() throws{
+        let testString = CrackStation().crack(password: "ae4f281df5a5d0ff3cad6371f76d5c29b6d953ec")
+        
+        XCTAssertEqual("B",testString)
+        XCTAssertNotNil(testString)
+    }
 }
