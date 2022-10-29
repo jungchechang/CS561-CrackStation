18a5fe6acc3a034d39ff6a4d364cc8a96b13016e
JungCheChang <jungche0408@gmail.com>
2022-10-28 19:39:43 -0700

    Update

--- Sources/CrackStation/Decrypter.swift
+++ Sources/CrackStation/Decrypter.swift
@@ -1,6 +1,5 @@
 public protocol Decrypter {
     init()
-
     /// Either returns the cracked plain-text password
     /// or, if unable to crack, then returns nil.
     /// - Parameter shaHash: The SHA-1 or SHA-256 digest that corresponds to the encrypted password.
